9fb17c2642ef279bd10f1676a1b5c2d093170b9c
Muhid Hossain <muhidhossain7@gmail.com>
2020-07-10 11:12:19 +0600

    learned strict mode

---
+++ Advance JavaScript/basicJavaScriptBoilerplate-master/basicJavaScriptBoilerplate-master/script.js
@@ -0,0 +1,35 @@
+// strict mode on full script
+// "use strict"
+
+// var name = "Muhid Hossain";
+// var age = 22;
+
+// var address = "Barisal";
+
+// console.log("My name is " + name + " and I am " + age + " years old!");
+
+// function aFunc() {
+//     job = "Student";
+//     console.log("A Function with Strict Mode Enabled");
+// }
+// aFunc();
+
+// console.log("I am a " + job + " And I am from " + address);
+
+// strict mode on specific function
+var name = "Muhid Hossain";
+var age = 22;
+
+address = "Barisal";
+
+console.log("My name is " + name + " and I am " + age + " years old!");
+
+function aFunc1() {
+    "use strict"
+    job = "Student";
+    console.log("A Function with Strict Mode Enabled");
+}
+
+aFunc1();
+
+console.log("I am a " + job + " And I am from " + address);
